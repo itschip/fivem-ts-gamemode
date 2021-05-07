a0b0443552e4c6bbd863bc104fe00ca29e2e603b
Christopher <onlygamernerd@gmail.com>
2021-05-07 17:21:57 +0200

    new class go brr

---
+++ server/player/player.service.ts
@@ -0,0 +1,19 @@
+class _PlayerService {
+  constructor() {
+    console.log('Player class started')
+  }
+
+  async getLevel(): Promise<number> {
+    const query = `SELECT level FROM users WHERE username = ?`
+    return 1;
+  }
+
+  async setLevel(level: number): Promise<void> {
+    const query = `UPDATE users SET level = ? WHERE username = ?`
+  }
+
+  async updateLevel(level: number): Promise<void> {
+    const currentLevel = await this.getLevel();
+    const newLevel = currentLevel + level;
+  }
+}
