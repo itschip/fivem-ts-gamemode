e3b87d72b4831ff3f7587e112c028521a84fa278
Christopher <onlygamernerd@gmail.com>
2021-05-07 15:43:40 +0200

    feat(client/utils): utils class

---
+++ client/utils/fivem.ts
@@ -0,0 +1,14 @@
+export class _Fivem {
+  Delay(ms: number) {
+    return new Promise((res) => setTimeout(res, ms))
+  }
+
+  clearArea(radius: number) {
+    const [x, y, z] = GetEntityCoords(PlayerPedId(), true);
+    ClearArea(x, y, z, radius, true, false, false, false);
+  }
+}
+
+const Fivem = new _Fivem();
+
+export default Fivem;
