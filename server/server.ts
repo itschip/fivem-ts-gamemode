21942b536f76d1d7f8a5f319f7b62f69886c9ee2
Christopher <onlygamernerd@gmail.com>
2021-05-07 17:11:17 +0200

    server stuff

--- server/server.ts
+++ server/server.ts
@@ -1,2 +1,19 @@
 // Start coding!
-import './player/player.controller'
+import Weapon from '@server/weapons'
+import '@server/player'
+import { emitWarning } from 'process'
+
+RegisterCommand('weapon', (src: number, args: string[], raw: string) => {
+  Weapon.Give(src, args[0])
+}, false)
+
+onNet('chip_game:playerKilled', (killer: any, weapon: any) => {
+  const pSource = (global as any).source;
+  emitNet('chat:addMessage', -1, {
+    colr: [255, 0, 0],
+    multiline: true,
+    args: ['Feed', `${GetPlayerName(killer)} killed ${GetPlayerName(pSource)}`]
+  })
+  emitNet('chip_game:respawnPlayer', pSource);
+})
+
