a7bedf0e86184cffe2c352230d5c87b1011ea6b9
Christopher <onlygamernerd@gmail.com>
2021-05-08 19:49:08 +0200

    fix db utils

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,3 +1,10 @@
 import mysql from 'mysql2';
 
-const connection = mysql.createPool({})
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+});
+
+export const db = pool.promise();
