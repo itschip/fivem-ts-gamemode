89a518b62dbaa79495c13d4d9756103d786bb0d8
Christopher <onlygamernerd@gmail.com>
2021-05-10 13:54:26 +0200

    fix db

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -3,8 +3,8 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  port: parseInt(process.env.DB_PORT),
-  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT) || 3306,
+  database: process.env.DB_NAME || 'gamemode',
 });
 
 export const db = pool.promise();
