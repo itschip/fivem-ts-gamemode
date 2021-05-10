a7ff2c748310789b924165d11ac46e8b48fffcdb
Christopher <onlygamernerd@gmail.com>
2021-05-10 13:55:02 +0200

    weird stuff

--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,49 @@
 // Start coding!
-import Weapon from '@server/weapons'
-import '@server/player'
-import '@server/account'
+import Weapon from '@server/weapons';
+import '@server/account';
 import Player from '@server/player';
 
-RegisterCommand('weapon', (src: number, args: string[], raw: string) => {
-  Weapon.Give(src, args[0])
-}, false)
+RegisterCommand(
+  'weapon',
+  (src: number, args: string[], raw: string) => {
+    Weapon.Give(src, args[0]);
+
+    for (const comp of weaponComps[args[0]]) {
+      Weapon.GiveComponent(src, args[0], comp);
+    }
+  },
+  false,
+);
 
 onNet('chip_game:playerKilled', (killer: any, weapon: any) => {
   const pSource = (global as any).source;
   emitNet('chat:addMessage', -1, {
     colr: [255, 0, 0],
     multiline: true,
-    args: ['Feed', `${GetPlayerName(killer)} killed ${GetPlayerName(pSource)}`]
-  })
-  emitNet('chip_game:respawnPlayer', pSource)
-})
+    args: ['Feed', `${GetPlayerName(killer)} killed ${GetPlayerName(pSource)}`],
+  });
+  emitNet('chip_game:respawnPlayer', pSource);
+});
 
+const weaponComps: any = {
+  WEAPON_PISTOL: [
+    'COMPONENT_AT_PI_FLSH',
+    'COMPONENT_PISTOL_CLIP_02',
+    'COMPONENT_AT_PI_SUPP_02',
+    'COMPONENT_PISTOL_VARMOD_LUXE',
+  ],
+  WEAPON_CARBINERIFLE: [
+    'COMPONENT_CARBINERIFLE_CLIP_03',
+    '	COMPONENT_AT_AR_FLSH',
+    'COMPONENT_AT_SCOPE_MEDIUM',
+    'COMPONENT_AT_AR_SUPP',
+    'COMPONENT_AT_AR_AFGRIP',
+    'COMPONENT_CARBINERIFLE_VARMOD_LUXE',
+  ],
+};
 
-RegisterCommand('givemoney', async () => {
-  const pSource: number = (global as any).source;
-
-  await Player.account.giveMoney(pSource, 100)
-}, false)
+onNet('getAllVehcilesChip', () => {
+  for (const vehicle of GetAllVehicles()) {
+    FreezeEntityPosition(vehicle, true);
+  }
+});
