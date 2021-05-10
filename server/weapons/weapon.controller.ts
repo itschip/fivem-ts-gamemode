217bf4f9c4e787e29e97a98d5fa693344968791b
Christopher <onlygamernerd@gmail.com>
2021-05-10 13:54:08 +0200

    update weapon controller

--- server/weapons/weapon.controller.ts
+++ server/weapons/weapon.controller.ts
@@ -3,6 +3,11 @@ export class _Weapon {
   Give(src: number, weaponName: string) {
     GiveWeaponToPed(GetPlayerPed(src.toString()), GetHashKey(weaponName), 250, false, true);
   }
+
+  GiveComponent(src: number, weapon: string, attachment: string) {
+    console.log("selected gun: ", GetSelectedPedWeapon(GetPlayerPed(src.toString())))
+    GiveWeaponComponentToPed(GetPlayerPed(src.toString()), GetHashKey(weapon), GetHashKey(attachment));
+  }
 }
 
 const Weapon = new _Weapon();
