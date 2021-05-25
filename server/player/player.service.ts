bc63a29d43114f28a4a55aa756a797d9f191b4d0
Christopher <onlygamernerd@gmail.com>
2021-05-25 14:39:18 +0200

    update player service

--- server/player/player.service.ts
+++ server/player/player.service.ts
@@ -1,6 +1,19 @@
+import { PlayerSourceMap } from '@server/player';
+
 class _PlayerService {
-  constructor() {
-    console.log('Player Service started!')
+  private readonly _source: number
+
+  constructor(src: number) {
+    console.log('Player Service started!');
+    this._source = src;
+  }
+
+  public getName() {
+    return PlayerSourceMap.get(this._source).username
+  }
+
+  public getIdentifier() {
+    return PlayerSourceMap.get(this._source).identifier;
   }
 
 }
