b3a69be5f750cf0f22b468c2002e9383b7ec3e0e
Christopher <onlygamernerd@gmail.com>
2021-05-27 20:48:21 +0200

    update server

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,14 @@
 // Start coding!
-import './admin'
+import './admin';
 import Weapon from '@server/weapons';
-
+import { weaponComponents } from '@shared/weapons';
 
 RegisterCommand(
   'weapon',
   (src: number, args: string[], raw: string) => {
     Weapon.Give(src, args[0]);
 
-    for (const comp of weaponComps[args[0]]) {
+    for (const comp of weaponComponents[args[0]]) {
       Weapon.GiveComponent(src, args[0], comp);
     }
   },
@@ -24,20 +24,3 @@ onNet('chip_game:playerKilled', (killer: any, weapon: any) => {
   });
   emitNet('chip_game:respawnPlayer', pSource);
 });
-
-const weaponComps: any = {
-  WEAPON_PISTOL: [
-    'COMPONENT_AT_PI_FLSH',
-    'COMPONENT_PISTOL_CLIP_02',
-    'COMPONENT_AT_PI_SUPP_02',
-    'COMPONENT_PISTOL_VARMOD_LUXE',
-  ],
-  WEAPON_CARBINERIFLE: [
-    'COMPONENT_CARBINERIFLE_CLIP_03',
-    '	COMPONENT_AT_AR_FLSH',
-    'COMPONENT_AT_SCOPE_MEDIUM',
-    'COMPONENT_AT_AR_SUPP',
-    'COMPONENT_AT_AR_AFGRIP',
-    'COMPONENT_CARBINERIFLE_VARMOD_LUXE',
-  ],
-};
